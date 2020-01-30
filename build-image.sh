cade9a911729d9352617ecf946f9846aea502e4c
Grim Kriegor <grimkriegor@krutt.org>
2020-01-30 01:02:41 +0000

    Prune version string

--- build-image.sh
+++ build-image.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-version=$(grep "ENV VERSION .*" Dockerfile | awk 'NF>1{print $NF}')
+version=$(grep "ENV VERSION .*" Dockerfile \
+  | awk 'NF>1 {print $NF}' \
+  | awk -F '-' '{print $1}')
 arch=$(uname -m)
 tag="$version"
 
 case "$arch" in
-  "armv*"   ) tag+="-armhf" ;;
-  "aarch64" ) tag+="-arm64" ;;
+  armv*   ) arch="armhf" ;;
+  aarch64 ) arch="arm64" ;;
+  *       ) arch=""      ;;
 esac
 
+if [ ! -z "$arch" ]; then
+  tag+="-$arch"
+fi
+
+docker build -t zalkeen/murmur:${arch:-latest} .
 docker build -t zalkeen/murmur:$tag .
 
